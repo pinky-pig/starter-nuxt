b52addc7e61f27763150b856b0308293d0fddf38
pinky-pig <2545589993@qq.com>
2024-10-22 19:44:12 +0800

    fix: 修改 dd 打卡的接口

--- server/api/dd.ts
+++ server/api/dd.ts
@@ -6,10 +6,19 @@ let is_run_daka_status = false
 export default defineEventHandler((event) => {
   const query = getQuery(event)
 
-  is_run_daka_status = Boolean(query.status === 'true')
-  return {
-    status: 'success', 
-    is_run_daka_status, 
-    time: formattedDate,
+  if (query.status !== undefined) {
+    is_run_daka_status = Boolean(query.status === 'true')
+    return {
+      status: 'success', 
+      is_run_daka_status, 
+      time: formattedDate,
+    }
+  }
+  else {
+    return {
+      status: 'success', 
+      is_run_daka_status, 
+      time: formattedDate,
+    }
   }
 })
